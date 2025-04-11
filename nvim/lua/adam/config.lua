443b43dca88dedc60b878dad62fcb22b929c5036
AdamWS17 <awsteppe@gmail.com>
2025-04-11 13:54:17 -0400

    small config tweaks

--- nvim/lua/adam/config.lua
+++ nvim/lua/adam/config.lua
@@ -1,6 +1,8 @@
 --[[     General Config     ]]
 vim.opt.list = true; --required for listchars
+
 vim.opt.tabstop = 4;
+vim.opt.softtabstop = 4;
 vim.opt.shiftwidth = 4;
 vim.opt.listchars = { --[[lead = '·',]] trail = '·', tab = [[│ ]]}
 ---vim.opt.expandtab = false;
@@ -15,6 +17,12 @@ vim.opt.wrap = false;
 vim.opt.scrolloff = 15;
 vim.opt.sidescrolloff = 20;
 vim.opt.splitright = true; --doesnt work??
+
+vim.opt.hlsearch = false;
+vim.opt.incsearch = true;
+vim.opt.signcolumn = yes;
+
+
 
 		--vim.opt.listchars = { space = '·', tab = '>~' }
 		--aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
