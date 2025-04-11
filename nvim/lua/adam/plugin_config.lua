eb0622f284c1fdb4b0979bf02d10acce5d9c9a8d
AdamWS17 <awsteppe@gmail.com>
2025-04-11 13:29:29 -0400

    should probably delete this file

--- nvim/lua/adam/plugin_config.lua
+++ nvim/lua/adam/plugin_config.lua
@@ -1,3 +1,4 @@
+--[[
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -5,5 +6,6 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+--]]
 
 
